2c2d0d9b77e8e76a70f059e6e70681efb46ccd5f
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2020-05-01 13:34:36 +0300

    Remove test package

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![CI](https://github.com/ravil23/lingualynda/workflows/Continious%20integration/badge.svg)
+![CI](https://github.com/ravil23/lingualynda/workflows/.github/workflows/ci.yml/badge.svg)
 
 # Lingua Lynda
 AI powered dialog system for learning languages
--- bot/main.go
+++ bot/main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ravil23/lingualynda/bot/wc"
 )
 
 func main() {
-	text := "To be, or not to be?"
-	counts := wc.WordCounts(text)
-	orderedCounts := wc.SortByValuesAndKeys(counts, true)
-	for _, elem := range orderedCounts {
-		fmt.Println(elem.Count, elem.Word)
-	}
+	fmt.Println("Hello! I am Lynda!")
 }
