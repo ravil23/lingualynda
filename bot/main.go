3b64c64b9decaaff87f50ad97ba4a7e5004d557d
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2020-05-01 15:58:40 +0300

    Init telegram bot

--- bot/main.go
+++ bot/main.go
@@ -1,45 +1,11 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/ravil23/lingualynda/bot/telegrambot"
 )
 
 func main() {
-	log.Printf("Initialize bot API...")
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
-	if err != nil {
-		log.Panic(err)
-	}
-
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	log.Print("Bot is listening updates...")
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		_, err = bot.Send(msg)
-		if err != nil {
-			log.Printf("ERROR: %v", err)
-		}
-	}
+	bot := telegrambot.NewBot()
+	bot.Init()
+	bot.Run()
 }
---
+++ bot/telegrambot/bot.go
@@ -0,0 +1,55 @@
+package telegrambot
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const timeout = 10
+
+type Bot struct {
+	api *tgbotapi.BotAPI
+}
+
+func NewBot() *Bot {
+	return &Bot{}
+}
+
+func (b *Bot) Init() {
+	log.Printf("Bot is initializing...")
+	var err error
+	b.api, err = tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Printf("Authorized on account %s", b.api.Self.UserName)
+}
+
+func (b *Bot) Run() {
+	log.Print("Bot is running...")
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = timeout
+
+	updates, err := b.api.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		_, err = b.api.Send(msg)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+		}
+	}
+}
