d14f22c9a2a47c265331b2d234ad8636efc94229
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2020-07-11 11:29:30 +0300

    Add lesson adverbs

--- telegrambot/collection/lesson/wordlist.go
+++ telegrambot/collection/lesson/wordlist.go
@@ -8,6 +8,10 @@ var VocabularyAdjectives = schema.NewVocabulary(
 	map[schema.Term][]schema.Translation{
 		"arrogant":   {"высокомерный"},
 		"cheerful":   {"жизнерадостный"},
+		"dishonest":  {"бесчестный"},
+		"edible":     {"съедобный"},
+		"grumpy":     {"сердитый", "ворчливый"},
+		"guilty":     {"виноватый"},
 		"foreign":    {"иностанный"},
 		"greasy":     {"жирный"},
 		"hazardous":  {"опасный"},
@@ -17,11 +21,18 @@ var VocabularyAdjectives = schema.NewVocabulary(
 	},
 )
 
+var VocabularyAdverbs = schema.NewVocabulary(
+	map[schema.Term][]schema.Translation{
+		"in advance": {"заранее", "заблаговременно", "наперед"},
+	},
+)
+
 var VocabularyNouns = schema.NewVocabulary(
 	map[schema.Term][]schema.Translation{
 		"betrayal":      {"предательство"},
 		"chariot":       {"колесница"},
 		"gossip":        {"сплетни"},
+		"guilt":         {"вина"},
 		"hide-and-seek": {"прятки"},
 		"leather":       {"кожанный материал"},
 		"manual labour": {"ручной труд"},
@@ -29,17 +40,20 @@ var VocabularyNouns = schema.NewVocabulary(
 		"nausea":        {"тошнота"},
 		"slave":         {"раб"},
 		"stroll":        {"прогулка"},
+		"tears":         {"слезы"},
 		"well":          {"колодец"},
 	},
 )
 
 var VocabularyVerbs = schema.NewVocabulary(
 	map[schema.Term][]schema.Translation{
+		"be in tears":  {"рыдать"},
 		"betray":       {"предать"},
 		"bury":         {"хоронить"},
 		"cost fortune": {"стоит целое состояние"},
 		"devote":       {"посвятить", "уделять"},
 		"get stuck":    {"застрять"},
+		"have a row":   {"ссориться"},
 		"resemble":     {"быть похожим"},
 	},
 )
@@ -51,6 +65,7 @@ var AllVocabularies []*schema.Vocabulary
 func init() {
 	VocabularyEngToRus = schema.NewEmptyVocabulary().
 		Update(VocabularyAdjectives).
+		Update(VocabularyAdverbs).
 		Update(VocabularyNouns).
 		Update(VocabularyVerbs)
 	VocabularyRusToEng = VocabularyEngToRus.MakeInvertedVocabulary()
